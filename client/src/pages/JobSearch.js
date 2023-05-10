f8e1c269d7baed464cd2a60095a86644ccb0a0ac
Jason (Jung Hyun) Yoo <jasonyoo3026@gmail.com>
2023-05-10 23:40:01 +1000

    added README and website image

--- client/src/pages/JobSearch.js
+++ client/src/pages/JobSearch.js
@@ -1,55 +1,25 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import { Grid, Header, Message, Segment } from 'semantic-ui-react';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Header, Message, Segment } from 'semantic-ui-react';
 
-// import JobSearch from '../components/JobSearch';
+import JobSearch from '../components/JobSearch';
 
-// const JobSearchPage = () => {
-//   return (
-//     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-//       <Grid.Column style={{ maxWidth: 500 }}>
-//         <Header as='h2' color='pink' textAlign='center'>
-//           Job Search
-//         </Header>
-//         <Segment stacked>
-//           <JobSearch />
-//         </Segment>
-//         <Message>
-//           <Link to="/">Back to Home</Link>
-//         </Message>
-//       </Grid.Column>
-//     </Grid>
-//   );
-// };
-
-// export default JobSearchPage;
-
-
-import React, { useState } from 'react';
-import { Button, Input } from 'semantic-ui-react';
-
-const JobSearch = () => {
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const handleChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const handleSearch = () => {
-    // Perform the search here using searchTerm
-  };
-
+const JobSearchPage = () => {
   return (
-    <div>
-      <Input 
-        type="text" 
-        placeholder="Enter job title, keywords or company name" 
-        value={searchTerm} 
-        onChange={handleChange} 
-      />
-      <Button onClick={handleSearch}>Search</Button>
-    </div>
+    <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 500 }}>
+        <Header as='h2' color='pink' textAlign='center'>
+          Job Search
+        </Header>
+        <Segment stacked>
+          <JobSearch />
+        </Segment>
+        <Message>
+          <Link to="/">Back to Home</Link>
+        </Message>
+      </Grid.Column>
+    </Grid>
   );
 };
 
-export default JobSearch;
+export default JobSearchPage;
