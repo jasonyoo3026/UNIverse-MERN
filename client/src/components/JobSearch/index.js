2a37fe53a0f181618f577ea9c6aab003c49ad757
Jason (Jung Hyun) Yoo <jasonyoo3026@gmail.com>
2023-05-11 22:03:10 +1000

    commented out the job search

--- client/src/components/JobSearch/index.js
+++ client/src/components/JobSearch/index.js
@@ -1,56 +1,63 @@
-import React, { useEffect, useState } from 'react';
+// import React, { useEffect, useState } from 'react';
+// require("dotenv").config();
 
-const JobSearch = () => {
-  const [jobs, setJobs] = useState([]);
-  const [error, setError] = useState(null);
+// const JobSearch = () => {
+//   const [jobs, setJobs] = useState([]);
+//   const [error, setError] = useState(null);
+//   const [searchTerm, setSearchTerm] = useState('');
+//   const [location, setLocation] = useState('');
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = 'https://linkedin-jobs-search.p.rapidapi.com/';
-      const options = {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-          'X-RapidAPI-Key': '4a098933a0msh29ac767534bb523p1ac00djsn2afd9101aa11',
-          'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com'
-        },
-        body: JSON.stringify({
-          search_terms: 'python programmer',
-          location: 'Chicago, IL',
-          page: '1'
-        })
-      };
+//   const handleSearch = async () => {
+//     const url = 'https://linkedin-jobs-search.p.rapidapi.com/';
+//     const options = {
+//       method: 'POST',
+//       headers: {
+//         'content-type': 'application/json',
+//         'X-RapidAPI-Key': process.env.REACT_APP_RAPIDAPI_KEY,
+//         'X-RapidAPI-Host': 'linkedin-jobs-search.p.rapidapi.com'
+//       },
+//       body: JSON.stringify({
+//         search_terms: searchTerm,
+//         location: location,
+//         page: '1'
+//       })
+//     };
 
-      try {
-        const response = await fetch(url, options);
-        const result = await response.json(); // Assuming that the API returns JSON
-        setJobs(result);
-      } catch (error) {
-        setError(error);
-        console.error(error);
-      }
-    };
+//     try {
+//       const response = await fetch(url, options);
+//       const result = await response.json();
+//       setJobs(result);
+//     } catch (error) {
+//       setError(error);
+//       console.error(error);
+//     }
+//   };
 
-    fetchData();
-  }, []);
+//   return (
+//     <div>
+//       <input 
+//         type="text"
+//         value={searchTerm}
+//         onChange={e => setSearchTerm(e.target.value)}
+//         placeholder="Job title"
+//       />
+//       <input 
+//         type="text"
+//         value={location}
+//         onChange={e => setLocation(e.target.value)}
+//         placeholder="Location"
+//       />
+//       <button onClick={handleSearch}>Search</button>
+//       {error && <div>Error: {error.message}</div>}
+//       {jobs.length === 0 && <div>Loading...</div>}
+//       {jobs.map((job, index) => (
+//         <div key={index}>
+//           <h2>{job.title}</h2>
+//           <p>{job.description}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!jobs) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <div>
-        <h1>Job Results</h1>
-        {jobs.map((job, index) => (
-          <div key={index}>
-            <h2>{job.title}</h2>
-            <p>{job.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  }
-};
-
-export default JobSearch;
+// export default JobSearch;
